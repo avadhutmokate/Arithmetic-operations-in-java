256e580e01602a40c4f7c7221d8e56422ae95340
Avadhut Mokate <mokateavadhut@gmail.com>
2025-06-20 23:29:44 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,24 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+
+        // Declare and initialize variables
+        int num1 = 15;
+        int num2 = 4;
+
+        // Perform arithmetic operations
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        // Print results
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
